9ec9a5ab9cb55cc6c0524aa1722a81fe3207914d
david amick <github@davidamick.com>
2013-06-06 15:46:02 -0700

    Organize and add tmux alias

--- install.sh
+++ install.sh
@@ -2,7 +2,21 @@
 
 ruby symlink.rb
 
-rm -rf ~/.vim/bundle
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vimrc.bundles +BundleInstall +qa
+rm -rf $HOME/.vim/bundle
+mkdir $HOME/.vim/bundle
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+vim -u $HOME/.vimrc.bundles +BundleInstall +qa
+
+if test -f $HOME/.profile
+then
+  PROFILE_FILE="$HOME/.profile"
+else
+  PROFILE_FILE="$HOME/.bash_profile"
+fi
+
+if ! grep -q "alias tmux=\"TERM=screen-256color-bce tmux\"" $PROFILE_FILE
+then
+  echo "alias tmux=\"TERM=screen-256color-bce tmux\"" >> $PROFILE_FILE
+fi
+
+source $PROFILE_FILE
--- symlink.rb
+++ symlink.rb
@@ -3,12 +3,9 @@ require 'fileutils'
 
 working_dir = File.expand_path(File.dirname(__FILE__))
 home_dir = File.expand_path("~")
-dot_files = Dir.glob(File.join(working_dir,"*"))
+dot_files = Dir.glob(File.join("#{working_dir}/linked_files","*"))
 
 dot_files.each do |filename|
-  next if filename =~ /symlink\.rb$/
-  next if filename =~ /README\.md$/
-
   sym_link = File.join(home_dir,".#{File.basename(filename)}")
 
   FileUtils.rm sym_link if File.symlink?(sym_link) || File.exist?(sym_link)
